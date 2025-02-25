3e953128a8e2214f6556cdf69bf0246c275b8308
NATroutter <natroutter@gmail.com>
2025-02-25 20:29:04 +0200

    improvements

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,4 @@
 import type { NextConfig } from "next";
-import {getHostname, getProtocol} from "@/lib/utils";
 
 const nextConfig: NextConfig = {
 	output: "standalone",
@@ -7,20 +6,7 @@ const nextConfig: NextConfig = {
 		POCKETBASE_ADDRESS: process.env.POCKETBASE_ADDRESS
 	},
 	images: {
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: 'cdn.myanimelist.net',
-				port: '',
-				pathname: '**',
-			},
-			{
-				protocol: getProtocol(process.env.POCKETBASE_ADDRESS),
-				hostname: getHostname(process.env.POCKETBASE_ADDRESS),
-				port: '',
-				pathname: '**',
-			},
-		],
+		unoptimized: true
 	}
 };
 
