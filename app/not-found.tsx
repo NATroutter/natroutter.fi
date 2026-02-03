d2a74fdcce11fb59da39cc7a33b9150dbd2f15a1
NATroutter <natroutter@gmail.com>
2026-02-03 22:42:13 +0200

    Upgraded 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,12 +1,37 @@
+import Image from "next/image";
+
 export default function NotFound() {
 	return (
 		<div className="flex min-h-screen flex-col items-center justify-center p-6">
-			<div className="max-w-md text-center">
-				<h1 className="text-6xl font-bold mb-4">404</h1>
-				<h2 className="text-2xl font-semibold mb-4">Page Not Found</h2>
-				<p className="text-muted-foreground mb-6">
-					The page you are looking for does not exist or has been moved.
-				</p>
+			<div className="flex flex-col items-center justify-center">
+				<div className="flex flex-col sm:flex-row m-auto relative w-full">
+
+					<div className="text-center flex flex-col xs:hidden">
+						<h1 className="text-4xl md:text-6xl font-bold md:mb-2">404</h1>
+						<h2 className="text-1xl md:text-2xl font-semibold md:mb-2">Page Not Found</h2>
+						<p className="text-muted-foreground mb-6">
+							The page you are looking for does not exist or has been moved.
+						</p>
+					</div>
+
+					<div className="px-5">
+						<Image
+							className="h-full w-full m-auto rounded-xl max-w-md hidden xxs:flex"
+							src="/images/confused.png"
+							alt="Not found"
+							sizes="100vw"
+							width={0}
+							height={0}
+						/>
+					</div>
+					<div className="text-center absolute left-7/12 right-0 hidden xs:flex flex-col">
+						<h1 className="text-4xl md:text-6xl font-bold md:mb-2">404</h1>
+						<h2 className="text-1xl md:text-2xl font-semibold md:mb-2">Page Not Found</h2>
+						<p className="text-muted-foreground mb-6">
+							The page you are looking for does not exist or has been moved.
+						</p>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
